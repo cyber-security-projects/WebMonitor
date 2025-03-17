1267d69485e3f670c3052115477656bafb987c79
marcos <marcosmiotto711@gmail.com>
2025-03-17 08:33:29 -0300

    sla

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    return jsonify({"message": "Bem-vindo ao Flask no Koyeb!"})
+    return jsonify({"message": "Bem-vindo caralho!"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
